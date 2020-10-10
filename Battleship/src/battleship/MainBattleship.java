6b6d16fcdd154c99c315edba0b49d25021da7713
Juan Esteban <42305023+Juanes165@users.noreply.github.com>
2020-10-10 16:35:43 -0500

    added ship class

--- Battleship/src/battleship/MainBattleship.java
+++ Battleship/src/battleship/MainBattleship.java
@@ -6,6 +6,7 @@ public class MainBattleship {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
 		EventQueue.invokeLater(new Runnable() {
 
 			@Override
@@ -15,6 +16,23 @@ public class MainBattleship {
 			}
 
 		});
+		*/
+		Ship ship = new Ship(2);
+		//Ship ship2 = new Ship(4);
+		ship.setShipPosition(6, 3, false);
+		//ship2.setShipPosition(10, 10, false);
+		System.out.println("Barco 1");
+		printMatrix(ship.getPosition());
+		//System.out.println("Barco 2");
+		//printMatrix(ship2.getPosition());
 	}
 
+	public static void printMatrix(int[][] matrix) {
+	    for (int row = 0; row < matrix.length; row++) {
+	        for (int col = 0; col < matrix[row].length; col++) {
+	            System.out.printf("%4d", matrix[row][col]);
+	        }
+	        System.out.println();
+	    }
+	}
 }
--- Battleship/src/battleship/Ship.java
+++ Battleship/src/battleship/Ship.java
@@ -2,19 +2,54 @@ package battleship;
 
 public class Ship {
 	private int[][] position;
-	private int[][] damagedParts;
+	private int[] damagedParts;
 	private int size;
 	
 	public Ship(int size) {
 		
-		position = new int[size][2];
-		damagedParts = new int[size][2];
+		this.position = new int[size][2];
+		this.damagedParts = new int[size];
 		this.size = size;
 		
 	}
 	
 	public void setShipPosition(int x, int y, boolean isVertical) {
-
-
+		
+		if(isVertical) {
+			if(size + y > 10) {
+				y = 11 - size;
+			}
+			//y
+			int n = y;
+			for(int i = 0; i < size; i++) {
+				position[i][1] = n;
+				n++;
+			}
+			//x
+			for(int i = 0; i < size; i++) {
+				position[i][0] = x;
+			}
+		}
+		else {
+			if(size + x > 10) {
+				x = 11 - size;
+			}
+			//x
+			int n = x;
+			for(int i = 0; i < size; i++) {
+				position[i][0] = n;
+				n++;
+			}
+			//y
+			for(int i = 0; i < size; i++) {
+				position[i][1] = y;
+			}
+		}
+		
+	}
+	
+	
+	public int[][] getPosition() {
+		return position;
 	}
 }
